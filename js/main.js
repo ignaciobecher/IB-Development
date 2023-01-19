43263a3acf2576253aee8b0c02226f55fed2419c
Ignacio <ignacio.becher@gmail.com>
2023-01-19 18:17:15 -0300

    js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,36 @@ const inputName = document.getElementById("name-input");
 const inputMail = document.getElementById("mail-input");
 const inputConsult = document.getElementById("consult-input");
 const btnSend = document.getElementById("btn-send");
+const confirmationWindow = document.getElementById("confirmation-window");
+const validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-btnSend.addEventListener("click", () => {
+//Funcion para limpiar campos
+function clearFields() {
+  inputName.value = "";
+  inputMail.value = "";
+  inputConsult.value = "";
+}
+
+//Funcion confirmar envio de formulario
+
+btnSend.addEventListener("click", (e) => {
+  e.preventDefault();
   let valueName = inputName.value;
   let valueMail = inputMail.value;
   let valueConsult = inputConsult.value;
 
-  if (valueName === "") {
-    window.alert("obligatorio");
-  } else {
-    console.log(valueName, valueMail, valueConsult);
+  if (valueName === "" || valueMail === "" || valueConsult === "") {
+    window.alert("Todos los campos son obligatorios");
+  } else if (
+    valueName.length >= 5 ||
+    valueMail.value.match(validRegex) ||
+    valueConsult.length >= 5
+  ) {
+    confirmationWindow.style.display = "block";
+    clearFields();
+    setTimeout(() => {
+      confirmationWindow.style.display = "none";
+    }, 7000);
   }
 });
