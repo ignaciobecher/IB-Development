45f23442e06c2d372aaee112b491284013bbaa01
Ignacio <ignacio.becher@gmail.com>
2023-01-20 12:23:22 -0300

    responsive 90%

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Constantes para validacion de formulario
 const inputName = document.getElementById("name-input");
 const inputMail = document.getElementById("mail-input");
 const inputConsult = document.getElementById("consult-input");
@@ -6,15 +7,29 @@ const confirmationWindow = document.getElementById("confirmation-window");
 const validRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+//Constantes para menu responsive
+const btnMenu = document.getElementById("btn-menu");
+const btnClose = document.getElementById("btn-close");
+const linksResponsive = document.getElementById("links-responsive");
+const headerResponsive = document.getElementById("responsive-header");
+
+btnClose.addEventListener("click", () => {
+  linksResponsive.style.display = "none";
+});
+
+btnMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  linksResponsive.style.display = "grid";
+  btnMenu.style.display = "none";
+});
+
+//FUNCION ENVIO DE FORMULARIO (VALIDACION)
 //Funcion para limpiar campos
 function clearFields() {
   inputName.value = "";
   inputMail.value = "";
   inputConsult.value = "";
 }
-
-//Funcion confirmar envio de formulario
-
 btnSend.addEventListener("click", (e) => {
   e.preventDefault();
   let valueName = inputName.value;
